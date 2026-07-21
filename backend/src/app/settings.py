2120378dcfee3a126153d8722b70a0551e648621
felmarlop <fmartin@bigml.com>
2026-07-21 18:05:33 +0200

    feat(celery): integrate Celery with Django

--- backend/src/app/settings.py
+++ backend/src/app/settings.py
@@ -143,6 +143,17 @@ USE_I18N = True
 USE_TZ = True
 
 
+# Celery
+CELERY_BROKER_URL = env("CELERY_BROKER_URL")
+CELERY_RESULT_BACKEND = env("CELERY_RESULT_BACKEND")
+
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
+
+CELERY_TIMEZONE = TIME_ZONE
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
